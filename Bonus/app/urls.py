eb4ec2e69423851a987485f5b8724ea38ea8c969
ronaldmoreno <maycol9ronald99@gmail.com>
2021-11-20 00:08:05 -0500

    clase 16

---
+++ Bonus/app/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from app.views import *
+
+urlpatterns = [
+    path('uno/', myfirstview),
+    path('dos/', mysecondview),
+]
--- Bonus/app/views.py
+++ Bonus/app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from app.models import *
+
 
 # Create your views here.
+def myfirstview(request):
+    data = {
+        'name': 'William',
+        'categories': Category.objects.all()
+
+    }
+    return render(request, 'index.html', data)
+
+def mysecondview(request):
+    data = {
+        'name': 'William',
+        'products': Product.objects.all()
+
+    }
+    return render(request, 'seconds.html', data)
